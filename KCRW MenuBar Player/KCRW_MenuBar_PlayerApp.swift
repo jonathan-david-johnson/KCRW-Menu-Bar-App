f12fe99aaccceb99b3aa670a38b8844d72b1a1c0
jonathan-david-johnson <jdj@alumni.clemson.edu>
2022-10-04 13:46:17 -0400

    Initial Commit

---
+++ KCRW MenuBar Player/KCRW_MenuBar_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KCRW_MenuBar_PlayerApp.swift
+//  KCRW MenuBar Player
+//
+//  Created by Jonathan Johnson on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct KCRW_MenuBar_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
